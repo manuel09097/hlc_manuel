a2f8729bc2ac5148d091b5559684ec50846b8cb6
manuel09097 <manuel09097@gmail.com>
2025-01-23 12:30:22 +0100

    ultimo

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 // Variables de conexión
-$servidor = "localhost"; // Nombre del servidor (por defecto localhost)
+$servidor = "localhost:3307"; // Nombre del servidor (por defecto localhost)
 $usuario = "root";       // Usuario (por defecto root en XAMPP)
 $contrasena = "";        // Contraseña (por defecto vacío en XAMPP)
 $nombre_base_datos = "estudiantes"; // Nombre de la base de datos
-$puerto=3307;
+
 
 // Establecer la conexión
 $conexion = mysqli_connect($servidor, $usuario, $contrasena, $nombre_base_datos);
