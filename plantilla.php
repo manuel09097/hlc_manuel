f0e2d7f106f31c085e4e59233e92be8ab2856dd6
manuel09097 <manuel09097@gmail.com>
2025-01-31 08:29:31 +0100

    plantilla

---
+++ plantilla.php
@@ -0,0 +1,27 @@
+<?php
+// Incluir el archivo de conexión (si es necesario)
+include('conexion.php');
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de Estudiantes filtrado por nombre</title>
+    
+    <!-- Link al CSS de Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+<?php
+// Código en PHPli.
+?>
+
+
+<!-- Scripts de Bootstrap -->
+     <!-- Agregar el script de Bootstrap 5 desde el CDN al final del body -->
+     <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
