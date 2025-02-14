876c146c8864ec2342b4fabda76686fe148a2c7b
manuel09097 <manuel09097@gmail.com>
2025-02-14 09:50:15 +0100

    login

--- Biblioteca/login.php
+++ Biblioteca/login.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="login-form-20/css/bootstrap.min.css">
 </head>
 <body class="bg-light">
     <div class="container d-flex justify-content-center align-items-center vh-100">
@@ -49,5 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </form>
         </div>
     </div>
+
+    <script src="login-form-20/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
